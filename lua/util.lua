55e388aff6bfc2863a727afe893b169060f0f191
Ralph Versteegen <teeemcee@gmail.com>
2015-07-08 01:15:32 +1200

    Add new Util module, w/ Util.seqRemove() to remove an item from a sequence

---
+++ lua/util.lua
@@ -0,0 +1,27 @@
+
+--
+--	actor.lua
+--	Generic utility functions not belonging anywhere else
+--
+
+local Util = {}
+
+--	Util.seqDelete() - removes an object from a numerical sequence, to go with
+--	table.insert and table.remove.
+--	Returns true on success, false if it didn't exist.
+function Util.seqRemove(seq, item)
+	local pos
+	for i,v in ipairs(seq) do
+		if v == item then
+			pos = i
+			break
+		end
+	end
+	if not pos then
+		return false
+	end
+	table.remove(seq, pos)
+	return true
+end
+
+return Util
